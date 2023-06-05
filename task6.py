759c2efe52969dc7f1a518e5e37ae1b0f831a944
Manhatai <131269530+Manhatai@users.noreply.github.com>
2023-06-05 13:34:55 +0200

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,7 +1,3 @@
 def upload_xml(x):
-    try:
         with open(x) as f:
             return xmltodict.parse(f.read())  # konwertuje .xml na python
-    except FileNotFoundError:
-        print("Nie znaleziono podanego pliku! Spróbuj ponownie.")
-        quit()
